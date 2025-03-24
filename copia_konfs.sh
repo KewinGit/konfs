2f027dd27581d58e7b1e63f8c19e33ab2f017926
Kewin <kevin.froster.personal@outlook.it>
2025-03-24 14:35:14 +0000

    ADD: scripts di copia incolla

---
+++ copia_konfs.sh
@@ -0,0 +1,6 @@
+# cp -R path_to_source path_to_destination/
+
+cp -rf ~/.config ~/konfs/
+cp -rf ~/.themes ~/konfs/
+cp -rf ~/.icons ~/konfs/
+cp -rf ~/.local/share/fonts ~/konfs/
---
+++ incolla_konfs.sh
@@ -0,0 +1,4 @@
+cp -rf ~/konfs/.config ~/
+cp -rf ~/konfs/.themes ~/
+cp -rf ~/konfs/.icons ~/
+cp -rf ~/konfs/.local/share/fonts ~/
