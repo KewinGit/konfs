e701a3db95836dafc88806d2df3e7afb1f31dfba
Kewin <kevin.froster.personal@outlook.it>
2025-03-25 08:16:35 +0000

    VAR: scripts copia incolla

--- copia_konfs.sh
+++ copia_konfs.sh
@@ -1,6 +1,12 @@
 # cp -R path_to_source path_to_destination/
 
+echo - Copio le cartelle delle configurazioni nel git repo
+echo
+
 cp -rf ~/.config ~/konfs/
 cp -rf ~/.themes ~/konfs/
 cp -rf ~/.icons ~/konfs/
 # cp -rf ~/.local/share/fonts ~/konfs/
+
+echo
+echo - copia_konfs.sh eseguito!
--- incolla_konfs.sh
+++ incolla_konfs.sh
@@ -1,4 +1,11 @@
+
+echo - Incollo le cartelle delle configurazioni nella home
+echo
+
 cp -rf ~/konfs/.config ~/
 cp -rf ~/konfs/.themes ~/
 cp -rf ~/konfs/.icons ~/
 # cp -rf ~/konfs/.local/share/fonts ~/
+
+echo
+echo - incolla_konfs.sh eseguito!
