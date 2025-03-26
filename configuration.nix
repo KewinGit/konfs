31a9792c4489bb6cb568bf34f2507811c4a9a5c9
Kewin <kevin.froster.personal@outlook.it>
2025-03-26 14:35:56 +0000

    VAR: bluetooth

--- configuration.nix
+++ configuration.nix
@@ -93,8 +93,17 @@
   };
 
   # Bluetooth
-  hardware.bluetooth.enable = true; # enables support for Bluetooth
-  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+
+  hardware.bluetooth = {
+    enable = true;              # enables support for Bluetooth
+    powerOnBoot = true;         # powers up the default Bluetooth controller on boot
+    settings = {
+      General = {
+        Experimental = "true";
+      };
+    };
+  };
+
   services.blueman.enable = true;
 
   hardware.logitech.wireless.enable = true;
